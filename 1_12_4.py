c4a13717f4415976a5dc846dd0edf286c22edcb8
PaLLadiYs <Mus21teru>
2021-09-19 15:31:28 +0300

    calcualtor for 1_12_4

---
+++ 1_12_4.py
@@ -0,0 +1,19 @@
+# calcualtor for 1_12 lesson ex.4
+f = str(input())
+
+if f == 'треугольник':
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    # calculator
+    p = (a + b + c) / 2
+    s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
+    print(s)
+elif f == 'прямоугольник':
+    a = int(input())
+    b = int(input())
+    print(a * b)
+elif f == 'круг':
+    r = int(input())
+    pi = 3.14
+    print(r ** 2 * pi)
